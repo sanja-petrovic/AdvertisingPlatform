1e9ff3d2dc09a36e9421086fe81594566b7928dc
Sanya <sanjica.petrovic7@gmail.com>
2022-08-10 00:15:33 +0200

    Filter design

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,11 +1,6 @@
-import React, { useState }  from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react'
 import './home.css'
 import '../common/_base.css'
-import {
-    BrowserRouter as Router,
-    Routes, Route, Link
-} from "react-router-dom"
 import dress from "../../resources/dress.jpg"
 import corgi from "../../resources/corgi.jpg"
 
@@ -15,34 +10,56 @@ class Home extends React.Component {
             <div className="home-content">
                 <p className="subtitle">Adverts</p>
                 <p className="description">Check out all available adverts below.</p>
-                <table className="advert-table">
+                <div className="dropdown-container">
+                    <div className="input-group mb-3">
+                        <select className="form-select">
+                            <option disabled selected hidden>Category</option>
+                            <option>Svi</option>
+                        </select>
+                        <input type="text" className="form-control" placeholder="Name"
+                        />
+                        <input type="number" className="form-control" placeholder="Price (min)"
+                        />
+                        <input type="number" className="form-control" placeholder="Price (max)"
+                        />
+                    </div>
+                    <input type="checkbox" id="mine-only"/>
+                    <label for="mine-only">Show mine only</label>
+                </div>
+                <table className="table">
                     <thead>
-                        <tr>
-                            <th>Image</th>
-                            <th>Title</th>
-                            <th>Price</th>
-                            <th>City</th>
-                            <th>Categories</th>
-                            <th className="button-col"></th>
-                        </tr>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>City</th>
+                        <th>Categories</th>
+                        <th className="button-col"></th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><img src={dress}/> </td>
-                            <td><b>Prom dress</b></td>
-                            <td>$249</td>
-                            <td>Amsterdam</td>
-                            <td>clothing</td>
-                            <td><button className="primary-button">Check out</button><button className="primary-button">Edit</button><button className="primary-button">Delete</button></td>
-                        </tr>
-                        <tr>
-                            <td><img src={corgi}/> </td>
-                            <td><b>Corgi puppy</b></td>
-                            <td>$1099</td>
-                            <td>Lisbon</td>
-                            <td>pets</td>
-                            <td><button className="primary-button">Check out</button><button className="primary-button">Edit</button><button className="primary-button">Delete</button></td>
-                        </tr>
+                    <tr>
+                        <td><img src={dress}/></td>
+                        <td><b>Prom dress</b></td>
+                        <td>$249</td>
+                        <td>Amsterdam</td>
+                        <td>clothing</td>
+                        <td>
+                            <button className="primary-button">Check out</button>
+                            <button className="primary-button">Edit</button>
+                            <button className="primary-button">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><img src={corgi}/></td>
+                        <td><b>Corgi puppy</b></td>
+                        <td>$1099</td>
+                        <td>Lisbon</td>
+                        <td>pets</td>
+                        <td>
+                            <button className="button primary-button">Check out</button>
+                        </td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
