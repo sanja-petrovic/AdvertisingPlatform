0d75295b2d8a7bccc0461b74dadb6a42ca7f3217
Sanya <sanjica.petrovic7@gmail.com>
2022-08-11 16:37:33 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- client/src/components/advertisement/SingleAdvertisement.js
+++ client/src/components/advertisement/SingleAdvertisement.js
@@ -1,3 +1,24 @@
+<<<<<<< Updated upstream
 class SingleAdvertisement extends React.Component {
 
 }
+=======
+import React from 'react'
+import './home.css'
+import '../common/_base.css'
+import NavigationBar from "../common/NavigationBar";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavigationBar/>
+                <div className="home-content">
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
+>>>>>>> Stashed changes
